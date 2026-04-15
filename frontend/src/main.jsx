3eb0b6a0341008f0f39901202020c937841fc14a
rslan123 <mahameedrslan@gmail.com>
2026-04-15 15:36:34 +0300

    Update routing and basename for school server

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,8 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  // The basename ensures all links are relative to your school folder
+  <BrowserRouter basename="/cgroup32/ReactProject/dist">
     <App />
   </BrowserRouter>,
 );
