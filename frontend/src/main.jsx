76af202533ae130c4ff14ec90b4f3bc5de2a51c4
rslan123 <mahameedrslan@gmail.com>
2026-06-29 09:09:58 +0300

    fix router basename for Vercel

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // The basename ensures all links are relative to your school folder
-  <BrowserRouter basename="/cgroup32/ReactProject/dist">
+  <BrowserRouter basename="/">
     <App />
   </BrowserRouter>,
 );
